f07df9afe02eec3e6315f1d9b923eb403e5e4560
Sasha Majorović <alexander.majorov@gmail.com>
2025-06-02 19:36:28 +0200

    Create Format.php

---
+++ src/Format.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace DashaMail
+{
+  enum Format: string
+  {
+      case XML   = 'xml'  ;
+      case JSON  = 'json' ;
+      case JSONP = 'jsonp';
+  }
+}
+
+#EOF#
