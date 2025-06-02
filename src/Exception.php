2cfdb248fef77e5fd5c465e630cc0303f634ca86
Sasha Majorović <alexander.majorov@gmail.com>
2025-06-02 18:31:54 +0200

    Create Exception.php

---
+++ src/Exception.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace DashaMail
+{
+  use RuntimeException;
+  use JsonException;
+  use Throwable;
+  
+  final class Exception extends RuntimeException
+  {
+      public function __construct(
+          string $message,
+          public ?array $response = null,
+          int $code = 0,
+          ?Throwable $previous = null,
+      ) {
+          parent::__construct($message, $code, $previous);
+      }
+  }
+}
+
+#EOF#
